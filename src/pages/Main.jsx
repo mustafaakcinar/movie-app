92219da6f1869ccf5d618d70abea146350ce1ce6
Mustafa Akçınar <mustafaakcinarr@gmail.com>
2025-03-15 17:34:58 +0300

    env, firebase edit

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import MovieCard from '../components/MovieCard'
 
 const Main = () => {
+
   return (
+    <>
     <Navbar />
+    <h1>MOVIE</h1>
+    <MovieCard/>
+    </>
   )
 }
 
