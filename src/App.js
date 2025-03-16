5fcd9a119553b055ae764d11a8cd7d64a3fc6069
Mustafa Akçınar <mustafaakcinarr@gmail.com>
2025-03-17 00:08:49 +0300

    formik and yup added in login page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 import AuthProvider from "./context/AuthProvider";
 import AppRouter from "./router/AppRouter";
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,127 @@
-import React from 'react'
+import { Avatar, Container, Grid, Typography } from "@mui/material";
+import React from "react";
+import LockIcon from "@mui/icons-material/Lock";
+import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
+import { Formik, Form } from "formik";
+import { object, string } from "yup";
+import logo from "../assets/img/cinema-background-csjrmkuhfvvumb2q.jpg";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const loginSchema = object({
+    password: string()
+      .required("Şifre Zorunludur")
+      .min(8, "Şifre en az 8 karakter olmalı")
+      .max(16, "Şifre en fazla 16 karakter içermelidir")
+      .matches(/[a-z]+/, "Şifre en az bir küçük harf içermelidir")
+      .matches(/[A-Z]+/, "Şifre en az bir küçük harf içermelidir")
+      .matches(
+        /[@$!%*?&]+/,
+        "Şifre en az bir özel karakter (@$!%*?&) içermelidir"
+      ),
 
-export default Login
+      email: string().email("Lütfen geçerli email giriniz").required()
+  });
+
+  return (
+    <Container maxWidth="lg">
+      <Grid
+        container
+        justifyContent="center"
+        direction="row-reverse"
+        sx={{
+          height: "100vh",
+          p: 2,
+        }}
+      >
+        <Grid item xs={12} mb={3}>
+          <Typography variant="h3" color="primary" align="center">
+            STOCK APP
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12} sm={10} md={6}>
+          <Avatar
+            sx={{
+              backgroundColor: "secondary.light",
+              m: "auto",
+              width: 40,
+              height: 40,
+            }}
+          >
+            <LockIcon size="30" />
+          </Avatar>
+          <Typography
+            variant="h4"
+            align="center"
+            mb={4}
+            color="secondary.light"
+          >
+            Login
+          </Typography>
+
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            onSubmit={(values, actions) => {
+              actions.resetForm();
+              actions.setSubmitting(false); //? isSubmitting (Boolean)
+            }}
+          >
+            {({
+              isSubmitting,
+              handleChange,
+              handleBlur,
+              values,
+              touched,
+              errors,
+            }) => (
+              <Form>
+                <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+                  <TextField
+                    label="Email"
+                    name="email"
+                    id="email"
+                    type="email"
+                    variant="outlined"
+                    onChange={handleChange}
+                    value={values.email}
+                    error={touched.email && Boolean(errors.email)}
+                    onBlur={handleBlur}
+                    helperText={errors.email}
+                  />
+                  <TextField
+                    label="password"
+                    name="password"
+                    id="password"
+                    type="password"
+                    variant="outlined"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.password}
+                    error={touched.password && Boolean(errors.password)}
+                    helperText={errors.password}
+                  />
+                  <Button
+                    variant="contained"
+                    type="submit"
+                    disabled={isSubmitting}
+                  >
+                    Submit
+                  </Button>
+                </Box>
+              </Form>
+            )}
+          </Formik>
+
+          <Box sx={{ textAlign: "center", mt: 2 }}>
+            <Link to="/register">Do you have not an account?</Link>
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default Login;
