d7257b02a9f76f233fa80fce6500982f8c20c7bb
Mustafa Akçınar <mustafaakcinarr@gmail.com>
2025-03-26 20:40:49 +0300

    moviedetails page edit and video added

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,7 +1,7 @@
 import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const MovieDetails = () => {
   const [movieDetails, setMovieDetails] = useState("");
@@ -20,44 +20,64 @@ const MovieDetails = () => {
       .get(movieDetailBaseUrl)
       .then((res) => setMovieDetails(res.data))
       .catch((err) => console.log(err));
+    axios
+      .get(videoUrl)
+      .then((res) => setVideoKey(res.data.results[0].key))
+      .catch((err) => console.log(err));
   }, []);
 
-  const { title, poster_path, vote_average } = movieDetails;
+  const {
+    title,
+    poster_path,
+    overview,
+    vote_average,
+    release_date,
+    vote_count,
+  } = movieDetails;
 
   return (
-    <Box sx={{
-      display:"flex",
-      justifyContent:"center",
-      mt:5
-    }}>
-      <Card
-        className="movie"
-        sx={{ maxWidth: 275, backgroundColor: "#E3F2FD" }}
-      >
-        <CardMedia
-          className="movieImg"
-          sx={{ height: 350, width: 275, objectFit: "cover" }}
-          image={poster_path ? baseImageUrl + poster_path : defaultImage}
-          title={title}
-          loading="lazy"
-        />
-        <CardContent
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <Typography gutterBottom variant="h5" component="div">
-            {title}
-          </Typography>
-        </CardContent>
-        {/* <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions> */}
-      </Card>
-    </Box>
+    <div className="md:container px-10 mx-auto py-5">
+      <h1 className="text-center text-white text-3xl">{title}</h1>
+      {/* {videoKey && <VideoSection videoKey={videoKey} />} */}
+      <div className="md:container flex justify-center px-10">
+        <div className="flex flex-col lg:flex-row max-w-6xl rounded-lg bg-gray-100 dark:bg-gray-dark-second shadow-lg">
+          <img
+            className=" lg:w-1/3 h-96 lg:h-[600px] object-cover rounded-t-lg md:rounded-none md:rounded-l-lg"
+            src={poster_path ? baseImageUrl + poster_path : defaultImage}
+            alt="poster"
+          />
+          <div className="p-6 flex flex-col justify-between">
+            <div>
+              <h5 className="text-gray-900 dark:text-gray-50 text-xl font-medium mb-2">
+                Overview
+              </h5>
+              <p className="text-gray-700 dark:text-gray-300  text-base mb-4">
+                {overview}
+              </p>
+            </div>
+            <ul className="rounded-lg border border-gray-400 text-gray-900 dark:text-gray-300">
+              <li className="px-6 py-2 border-b border-gray-400 w-full rounded-t-lg">
+                {"Release Date : " + release_date}
+              </li>
+              <li className="px-6 py-2 border-b border-gray-400 w-full">
+                {"Rate : " + vote_average}
+              </li>
+              <li className="px-6 py-2 border-b border-gray-400 w-full">
+                {"Total Vote : " + vote_count}
+              </li>
+              <li className="px-6 py-2 border-gray-400 w-full rounded-t-lg">
+                <Link
+                  to={-1}
+                  className="text-blue-600 hover:text-blue-700 transition duration-300 ease-in-out mb-4"
+                >
+                  Go Back
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
